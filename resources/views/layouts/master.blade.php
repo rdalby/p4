2c0946d6ea953d134db9cea5b060b939c4ff9b68
Rachel Dalby <rdalby@hsph.harvard.edu>
2019-05-14 13:49:38 -0400

    changed css layout and main layout for better readabilty

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,16 +3,49 @@
 <head>
     <title>@yield('title')</title>
     <meta charset='utf-8'>
-    <link href={{ asset('/css/p4.css') }} rel='stylesheet'>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 
     @yield('head')
 </head>
-<body>
-<header>
-    <a href='/'><img src={{ asset('images/p4-logo.jpg') }} id='logo' alt='p4 logo' class='logo'></a>
-</header>
 
-<section>
+<style>
+    body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
+    body {font-size:16px;}
+    .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
+    .w3-half img:hover{opacity:1}
+</style>
+
+
+<body>
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-teal w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+    <a href="javascript:void(0)" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
+    <div class="w3-container">
+        <h3 class="w3-padding-64"><a href='/'><img src={{ asset('images/p4-logo-small.jpg') }} id='logo' alt='p4 logo' class='logo'></a></h3>
+    </div>
+    <div class="w3-bar-block">
+        <a href="#" class="w3-bar-item w3-button w3-hover-white">Home</a>
+        <a href="#showcase"  class="w3-bar-item w3-button w3-hover-white">Search</a>
+        <a href="#services"  class="w3-bar-item w3-button w3-hover-white">Playlists</a>
+        <a href="#designers" class="w3-bar-item w3-button w3-hover-white">Create-Update</a>
+        <a href="#packages" class="w3-bar-item w3-button w3-hover-white">Accounts</a>
+        <a href="#contact" class="w3-bar-item w3-button w3-hover-white">Login</a>
+    </div>
+</nav>
+
+
+!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:340px;margin-right:40px">
+
+    <!-- Header -->
+    <div class="w3-container" style="margin-top:80px" id="showcase">
+        <h1 class="w3-jumbo"><b>Media</b></h1>
+        <h1 class="w3-xxxlarge w3-text-teal"><b>Showcase</b></h1>
+        <hr style="width:50px;border:5px solid teal" class="w3-round">
+    </div>
+
+    <section>
     <table>
         <tr>
             @yield('content')
@@ -21,6 +54,8 @@
     </table>
 
 </section>
+</div>
+
 
 <footer>
     &copy; {{ date('Y') }}
